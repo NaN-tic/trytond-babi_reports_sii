1841036da87e4f1d6c471091b0feb7b03512cfdc
Marina Nicolau <marina.nicolau@nan-tic.com>
2019-12-10 13:13:31 +0100

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# This file is part babi_reports_sii module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from .test_babi_reports_sii import suite
+
+__all__ = ['suite']
